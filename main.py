097f8c6dc34bf028fc9977d65ff79c64d4f76ed2
Evan White <54874186+emwhite3@users.noreply.github.com>
2021-08-10 01:18:28 +0000

    Added class that handles on ready events. Added guild token

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import os
 import discord
 
 TOKEN = os.environ['DISCORD_TOKEN']
+GUILD = os.environ['DISCORD_GUILD']
 
 # this is an object that represents a connection to discord,
 # handles events, states and Discord's API
 client = discord.Client()
 
-@client.event
-async def on_ready():
-  print(f'{client.user} has connected to Discord!')
+class CustomClient(discord.Client):
+  async def on_ready(self):
+    print(f'{self.user} has connected to Discord!')
 
+client = CustomClient()
 client.run(TOKEN)
